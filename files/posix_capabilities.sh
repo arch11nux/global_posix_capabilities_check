eba2ee68bacfb083ac050686445495e34119ad3b
N / A <t27db5k2@PYKQ2971iMM9.idesjardins.com>
2020-05-26 10:35:57 -0400

    upload the posix script

--- files/posix_capabilities.sh
+++ files/posix_capabilities.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#A script to collect security information from a Linux host
+#Posix Capabilities checker
 version="version 1"
 
 report=/tmp/global_posix_capabilities_check.report.out
@@ -9,12 +9,10 @@ format=$export/global_posix_capabilities_check-export
 header()
 {
 echo -e "\n\e[00;33m#########################################################\e[00m" 
-echo -e "Collect security information from a Linux host" 
+echo -e "Posix Capabilities checker" 
 echo -e "\e[00;33m#########################################################\e[00m" 
 echo -e # $version\n" 
 }
-
-binarylist='aria2c\|arp\|ash\|awk\|base64\|bash\|busybox\|cat\|chmod\|chown\|cp\|csh\|curl\|cut\|dash\|date\|dd\|diff\|dmsetup\|docker\|ed\|emacs\|env\|expand\|expect\|file\|find\|flock\|fmt\|fold\|ftp\|gawk\|gdb\|gimp\|git\|grep\|head\|ht\|iftop\|ionice\|ip$\|irb\|jjs\|jq\|jrunscript\|ksh\|ld.so\|ldconfig\|less\|logsave\|lua\|make\|man\|mawk\|more\|mv\|mysql\|nano\|nawk\|nc\|netcat\|nice\|nl\|nmap\|node\|od\|openssl\|perl\|pg\|php\|pic\|pico\|python\|readelf\|rlwrap\|rpm\|rpmquery\|rsync\|ruby\|run-parts\|rvim\|scp\|script\|sed\|setarch\|sftp\|sh\|shuf\|socat\|sort\|sqlite3\|ssh$\|start-stop-daemon\|stdbuf\|strace\|systemctl\|tail\|tar\|taskset\|tclsh\|tee\|telnet\|tftp\|time\|timeout\|ul\|unexpand\|uniq\|unshare\|vi\|vim\|watch\|wget\|wish\|xargs\|xxd\|zip\|zsh'
 
 system_info()
 {
@@ -27,112 +25,58 @@ if [ "$hostnamed" ]; then
   echo -e "\n" 
 fi
 
-#search all *-release files for version info
-release=`cat /etc/*-release 2>/dev/null`
-if [ "$release" ]; then
-  echo -e "\e[00;31m[-] Specific release information:\e[00m\n$release"
-  echo -e "\n" 
-fi
-
 }
 
-user_info()
-{
-echo -e "\e[00;33m### USER/GROUP ##########################################\e[00m" 
-
-#all root accounts (uid 0)
-superman=`grep -v -E "^#" /etc/passwd 2>/dev/null| awk -F: '$3 == 0 { print $1}' 2>/dev/null`
-if [ "$superman" ]; then
-  echo -e "\e[00;31m[-] Super user account(s):\n$superman"
-  echo -e "\n"
-fi
-
-#pull out vital sudoers info
-sudoers=`grep -v -e '^$' /etc/sudoers 2>/dev/null |grep -v "#" 2>/dev/null`
-if [ "$sudoers" ]; then
-  echo -e "\e[00;31m[-] Sudoers configuration (condensed):$sudoers"
-  echo -e "\n"
-fi
-
-if [ "$export" ] && [ "$sudoers" ]; then
-  mkdir $format/etc-export/ 2>/dev/null
-  cp /etc/sudoers $format/etc-export/sudoers 2>/dev/null
-fi
-
-#is root permitted to login via ssh
-sshrootlogin=`grep "PermitRootLogin " /etc/ssh/sshd_config 2>/dev/null | grep -v "#" | awk '{print  $2}'`
-if [ "$sshrootlogin" = "yes" ]; then
-  echo -e "\e[00;31m[-] Root is allowed to login via SSH:" ; grep "PermitRootLogin " /etc/ssh/sshd_config 2>/dev/null | grep -v "#" 
-  echo -e "\n"
-fi
-
-}
-
-environmental_info()
-{
-echo -e "\e[00;33m### ENVIRONMENTAL #######################################\e[00m" 
-
-#check if selinux is enabled
-sestatus=`sestatus 2>/dev/null`
-if [ "$sestatus" ]; then
-  echo -e "\e[00;31m[-] SELinux seems to be present:\e[00m\n$sestatus"
-  echo -e "\n"
-fi
-
-#current umask value with both octal and symbolic output
-umaskvalue=`umask -S 2>/dev/null & umask 2>/dev/null`
-if [ "$umaskvalue" ]; then
-  echo -e "\e[00;31m[-] Current umask value:\e[00m\n$umaskvalue"
-  echo -e "\n"
-fi
-
-#umask value as in /etc/login.defs
-umaskdef=`grep -i "^UMASK" /etc/login.defs 2>/dev/null`
-if [ "$umaskdef" ]; then
-  echo -e "\e[00;31m[-] umask value as specified in /etc/login.defs:\e[00m\n$umaskdef"
-  echo -e "\n"
-fi
-
-}
 
 interesting_files()
 {
 echo -e "\e[00;33m### INTERESTING FILES ####################################\e[00m" 
 
-#search for suid files
-allsuid=`find / -perm -4000 -type f 2>/dev/null`
-findsuid=`find $allsuid -perm -4000 -type f -exec ls -la {} 2>/dev/null \;`
-if [ "$findsuid" ]; then
-  echo -e "\e[00;31m[-] SUID files:\e[00m\n$findsuid" 
+#list all files with POSIX capabilities set along with there capabilities
+fileswithcaps=`getcap -r / 2>/dev/null || /sbin/getcap -r / 2>/dev/null`
+if [ "$fileswithcaps" ]; then
+  echo -e "\e[00;31m[+] Files with POSIX capabilities set:\e[00m\n$fileswithcaps"
   echo -e "\n"
 fi
 
-if [ "$export" ] && [ "$findsuid" ]; then
-  mkdir $format/suid-files/ 2>/dev/null
-  for i in $findsuid; do cp $i $format/suid-files/; done 2>/dev/null
+if [ "$export" ] && [ "$fileswithcaps" ]; then
+  mkdir $format/files_with_capabilities/ 2>/dev/null
+  for i in $fileswithcaps; do cp $i $format/files_with_capabilities/; done 2>/dev/null
 fi
 
-#list of 'interesting' suid files
-intsuid=`find $allsuid -perm -4000 -type f -exec ls -la {} \; 2>/dev/null | grep -w $binarylist 2>/dev/null`
-if [ "$intsuid" ]; then
-  echo -e "\e[00;33m[+] Possibly interesting SUID files:\e[00m\n$intsuid" 
+#searches /etc/security/capability.conf for users associated capapilies
+userswithcaps=`grep -v '^#\|none\|^$' /etc/security/capability.conf 2>/dev/null`
+if [ "$userswithcaps" ]; then
+  echo -e "\e[00;33m[+] Users with specific POSIX capabilities:\e[00m\n$userswithcaps"
   echo -e "\n"
 fi
 
-#lists world-writable suid files
-wwsuid=`find $allsuid -perm -4002 -type f -exec ls -la {} 2>/dev/null \;`
-if [ "$wwsuid" ]; then
-  echo -e "\e[00;33m[+] World-writable SUID files:\e[00m\n$wwsuid" 
-  echo -e "\n"
+if [ "$userswithcaps" ] ; then
+#matches the capabilities found associated with users with the current user
+matchedcaps=`echo -e "$userswithcaps" | grep \`whoami\` | awk '{print $1}' 2>/dev/null`
+	if [ "$matchedcaps" ]; then
+		echo -e "\e[00;33m[+] Capabilities associated with the current user:\e[00m\n$matchedcaps"
+		echo -e "\n"
+		#matches the files with capapbilities with capabilities associated with the current user
+		matchedfiles=`echo -e "$matchedcaps" | while read -r cap ; do echo -e "$fileswithcaps" | grep "$cap" ; done 2>/dev/null`
+		if [ "$matchedfiles" ]; then
+			echo -e "\e[00;33m[+] Files with the same capabilities associated with the current user (You may want to try abusing those capabilties):\e[00m\n$matchedfiles"
+			echo -e "\n"
+			#lists the permissions of the files having the same capabilies associated with the current user
+			matchedfilesperms=`echo -e "$matchedfiles" | awk '{print $1}' | while read -r f; do ls -la $f ;done 2>/dev/null`
+			echo -e "\e[00;33m[+] Permissions of files with the same capabilities associated with the current user:\e[00m\n$matchedfilesperms"
+			echo -e "\n"
+			if [ "$matchedfilesperms" ]; then
+				#checks if any of the files with same capabilities associated with the current user is writable
+				writablematchedfiles=`echo -e "$matchedfiles" | awk '{print $1}' | while read -r f; do find $f -writable -exec ls -la {} + ;done 2>/dev/null`
+				if [ "$writablematchedfiles" ]; then
+					echo -e "\e[00;33m[+] User/Group writable files with the same capabilities associated with the current user:\e[00m\n$writablematchedfiles"
+					echo -e "\n"
+				fi
+			fi
+		fi
+	fi
 fi
-
-#lists world-writable suid files owned by root
-wwsuidrt=`find $allsuid -uid 0 -perm -4002 -type f -exec ls -la {} 2>/dev/null \;`
-if [ "$wwsuidrt" ]; then
-  echo -e "\e[00;33m[+] World-writable SUID files owned by root:\e[00m\n$wwsuidrt" 
-  echo -e "\n"
-fi
-
 
 }
 
@@ -145,8 +89,6 @@ call_each()
 {
   header
   system_info
-  user_info
-  environmental_info
   interesting_files
   footer
 }
